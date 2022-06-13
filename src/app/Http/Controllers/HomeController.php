b42169c27c0c03f10f9d48ab498b47454082e7dc
John Doe <https://johngit002@github.com>
2022-06-13 17:14:58 +0300

    Controllers

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Services\PageService;
-use FeedrssFeeding\Models\Category;
-use FeedrssFeeding\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
